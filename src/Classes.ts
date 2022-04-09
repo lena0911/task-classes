9b2ce107aadee22af2e2b10421336a5695832067
lena0911 <lenagolovkina09@gmail.com>
2022-04-09 19:02:36 +0300

    Задание по task-classes

--- src/Classes.ts
+++ src/Classes.ts
@@ -5,4 +5,20 @@
 - Класс должен иметь метод increment, который увеличивает counter на 1 и возвращает результат
 - Класс должен иметь метод decrement, который уменьшает counter на 1 и возвращает результат
  */
-export class Counter {}
+export class Counter {
+    counter: number = 0;
+
+    constructor(start: number = 0) {
+        this.counter = start;
+    }
+
+    increment(counter: number) {
+        this.counter++;
+        return this.counter;
+    }
+
+    decrement(counter: number) {
+        this.counter--;
+        return this.counter;
+    }
+}
